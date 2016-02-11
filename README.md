3d4893d04832f0e516068a73b6269d15ced69024
Walter Mattingly <waltdakind@gmail.com>
2016-02-11 15:26:20 -0500

    initial site files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # week-3-game
-Games for week three homework
+Game for week three homework
+
+# Grateful Dead hang man
+#basis for words will be band members
+#albums, songs, or people, places,  
+# venues that are part of Grateful Dead history
+
---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+  window.onload = function () {
+//Grateful Dead themed hangman
+  	var alphabet = ['a', 'b', 'c', 'd',
+  				 	'e', 'f', 'g',
+   					'h','i', 'j', 'k', 
+   					'l','m', 'n', 'o', 'p', 
+   					'q', 'r', 's',
+   					't', 'u', 'v',
+    				'w', 'x', 'y', 
+    				'z'];
+  // now i know my abc's
+  	var puzzleAnswers = ["Box of Rain", "Robert Hunter",
+  						 "John Barlow", "Pigpen", "Touch of Grey",
+  						 "Money for Gasoline", "Bob Dylan", 
+  						 "When I Paint My Masterpiece", "Estimated Prophet",
+  						 "Eyes of the World", "China Cat Sunflower",
+  						 "Vince Welnick", "Bruce Hornsby", "Dark Star",
+  						 "Aoxomoxoa", "American Beauty", "Donna Jean Godchaux",
+  						 "Ripple", "Comes A Time", "Black Muddy River",
+  						 "Brent Mydland", "Nassau Coliseum", 
+  						 ]
+
+
+
+
+
+
+
+}
